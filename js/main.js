8d35ccee5c0f3c720e649e344ea52f2ea5aa37c4
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-09-22 18:51:06 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    /* $('.intro__slider').slick({
+        dots: true,
+        vertical: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        //verticalSwiping: true,
+        arrows: false,
+
+      }); */
+
+});

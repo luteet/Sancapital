080566fe202167b96e982602768c2b15cd21900d
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-09-24 16:36:03 +0300

    v1.0

--- js/main.js
+++ js/main.js
@@ -9,6 +9,37 @@ $(function(){
         arrows: false,
 
       }); */
+      $('.info-business__slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
 
       $('.burger').on('click', function() {
         $(this).toggleClass('_active');
